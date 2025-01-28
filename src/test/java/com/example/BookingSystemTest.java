d4ce3541734063f246caa7c3a5cedba113661859
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-01-28 14:59:12 +0100

    Add tests for getAvailableRooms

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -4,16 +4,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 class BookingSystemTest {
     Room room = new Room("1337", "Penthouse");
+    Room room2 = new Room("2025", "Terrace");
+    Room room3 = new Room("1969", "Moon");
     RoomRepositoryMock roomRepositoryMock = new RoomRepositoryMock();
     TimeProviderMock timeProviderMock = new TimeProviderMock();
     LocalDateTime startTime = LocalDateTime.of(2025, 2, 15, 13, 30);
     LocalDateTime endTime = LocalDateTime.of(2025, 2, 16, 14, 10);
     LocalDateTime pastTime = LocalDateTime.of(2025, 1, 16, 12, 0);
+    LocalDateTime futureTime = LocalDateTime.of(2025, 3, 16, 15, 0);
     NotificationsServiceMock notificationsServiceMock = new NotificationsServiceMock();
     BookingSystem bookingSystem = new BookingSystem(timeProviderMock, roomRepositoryMock, notificationsServiceMock);
 
@@ -21,7 +27,7 @@ class BookingSystemTest {
     @DisplayName("Available room should return true when booking")
     void availableRoomShouldReturnTrueWhenBooking() {
         roomRepositoryMock.save(room);
-        boolean availableRoom =  bookingSystem.bookRoom("1337", startTime, endTime);
+        boolean availableRoom = bookingSystem.bookRoom("1337", startTime, endTime);
         assertThat(availableRoom).isEqualTo(true);
     }
 
@@ -95,5 +101,50 @@ class BookingSystemTest {
             bookingSystem.bookRoom("1337", endTime, startTime);
         });
         assertThat(exception.getMessage()).isEqualTo("Sluttid måste vara efter starttid");
+    }
+
+    @Test
+    @DisplayName("Showing available rooms requires valid startTime")
+    void showingAvailableRoomsRequiresValidStartTime() {
+        roomRepositoryMock.save(room);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bookingSystem.getAvailableRooms(null, endTime);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo("Måste ange både start- och sluttid");
+    }
+
+    @Test
+    @DisplayName("Showing available rooms requires valid endTime")
+    void showingAvailableRoomsRequiresValidEndTime() {
+        roomRepositoryMock.save(room);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bookingSystem.getAvailableRooms(startTime, null);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo("Måste ange både start- och sluttid");
+    }
+
+    @Test
+    @DisplayName("Showing available rooms requires startTime to be before endTime ")
+    void showingAvailableRoomsRequiresStartTimeToBeBeforeEndTime() {
+        roomRepositoryMock.save(room);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bookingSystem.getAvailableRooms(endTime, startTime);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Sluttid måste vara efter starttid");
+    }
+
+    @Test
+    @DisplayName("Showing available rooms should get unbooked rooms")
+    void showingAvailableRoomsShouldGetUnbookedRooms() {
+        roomRepositoryMock.save(room);
+        roomRepositoryMock.save(room2);
+        roomRepositoryMock.save(room3);
+
+        bookingSystem.bookRoom("1337", startTime, endTime);
+
+        List<Room> availableRooms = bookingSystem.getAvailableRooms(startTime, endTime);
+        assertThat(availableRooms).isEqualTo(List.of(room2, room3));
     }
 }
