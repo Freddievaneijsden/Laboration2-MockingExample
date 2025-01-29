99bc81de586ecc74eaba61e3e067dbdc2a8d828c
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-01-29 08:47:28 +0100

    Add fixed constant TimeProviderMock

--- src/test/java/com/example/TimeProviderMock.java
+++ src/test/java/com/example/TimeProviderMock.java
@@ -3,9 +3,9 @@ package com.example;
 import java.time.LocalDateTime;
 
 public class TimeProviderMock implements TimeProvider {
-
+    LocalDateTime FIXED_PRESENT_TIME = LocalDateTime.of(2025, 2, 14, 13, 30);
     @Override
     public LocalDateTime getCurrentTime() {
-        return LocalDateTime.now();
+        return FIXED_PRESENT_TIME;
     }
 }
