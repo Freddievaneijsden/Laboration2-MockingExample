71bae504b2b2651654405228e527785af01b47c3
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-01-31 08:41:22 +0100

    Implement sendPaymentConfirmation

--- src/test/java/com/example/payment/EmailServiceMock.java
+++ src/test/java/com/example/payment/EmailServiceMock.java
@@ -3,5 +3,14 @@ package com.example.payment;
 public class EmailServiceMock implements EmailService {
     @Override
     public void sendPaymentConfirmation(String mail, double amount) {
+        if (mail == null) {
+            throw new IllegalArgumentException("Mail must not be null");
+        }
+        else if (amount < 0) {
+            throw new IllegalArgumentException("Amount must not be negative");
+        }
+        else {
+            System.out.println("Email sent to " + mail + " confirming payment of " + amount);
+        }
     }
 }
